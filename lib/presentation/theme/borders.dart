ac30ce7d54a9fa90125dbfb11d4d609af8f1913f
Andre Hui <andrehui09@gmail.com>
2023-02-22 18:36:59 -0800

    feat: added error message to fields feat: implemented readOnly states clean: moved duplicate styles

--- lib/presentation/theme/borders.dart
+++ lib/presentation/theme/borders.dart
@@ -1,9 +1,11 @@
 part of 'constants.dart';
 
 class CollActionBorderStyles {
-
   //Border of input fields to avoid contrast problems
-  static BorderSide inputBorderSide = BorderSide(
-    color: Colors.grey[200]!
+  static BorderSide inputBorderSide = BorderSide(color: Colors.grey[200]!);
+
+  static OutlineInputBorder formFieldBorderSide = const OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(5)),
+    borderSide: BorderSide(color: Color(0x80707070)),
   );
 }
