1dc58c8357df455a85fa8f67ce77825e907c4d68
Andre Hui <andre.tm.hui@gmail.com>
2023-05-29 23:04:25 -0700

    feat: create profile objects

---
+++ lib/domain/profile/profile.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'profile.freezed.dart';
+
+@freezed
+class Profile with _$Profile {
+  const Profile._();
+
+  const factory Profile({
+    required String userId,
+    required String firstName,
+    required String lastName,
+    required String avatar,
+    String? bio,
+  }) = _Profile;
+
+  String get fullName => '$firstName $lastName';
+}
